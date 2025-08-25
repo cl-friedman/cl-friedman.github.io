bacea44927366e6e7d4577e7a1b8fbf9aafedee8
cl-friedman <cl.friedman@berkeley.edu>
2025-08-25 19:59:59 +0000

    Deploying to gh-pages from @ cl-friedman/cl-friedman.github.io@08c2563ae7fff58501b76ac5ee6cd38a2649eb99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
+  },{id: "nav-cv-experience",
+          title: "cv/experience",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
